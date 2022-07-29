4b8f713859486f4ef3ed5637945d03c67f7a1fda
jefri ng <jefri.sae-tem@varmeego.com>
2022-07-29 16:33:24 +0800

    redux example

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import * as React from 'react';
+import { Provider } from 'react-redux';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './redux/store/configureStore';
+
+
+const RNRedux = () => (
+    <Provider store = { configureStore }>
+      <App />
+    </Provider>
+  )
+  
+  AppRegistry.registerComponent(appName, () => RNRedux);
