f44189ad513b8960064d71bd151fd6b1a1db297c
jefri ng <ajefri7608@gmail.com>
2022-08-26 16:03:47 +0800

    login layout v2

---
+++ src/view/component/common/Divider.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, View, Image, Text} from 'react-native';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+export default function Divider() {
+  return (
+    <View style={styles.dividerContainer}>
+      <View style={styles.divider}></View>
+      <Text style={{paddingHorizontal: 10}}>In Process</Text>
+      <View style={styles.divider}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  dividerContainer: {
+    flexDirection: 'row',
+    display: 'flex',
+    paddingVertical: 10,
+  },
+  divider: {
+    flex: 3,
+    height: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: 'grey',
+    alignSelf: 'center',
+  },
+});
