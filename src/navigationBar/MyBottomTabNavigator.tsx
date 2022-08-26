084b78ad1fc79fb0ff2dfb870d745e070fd15df5
jefri ng <ajefri7608@gmail.com>
2022-08-26 12:08:47 +0800

    login page layout

--- src/navigationBar/MyBottomTabNavigator.tsx
+++ src/navigationBar/MyBottomTabNavigator.tsx
@@ -115,26 +115,6 @@ const MyBottomTabNavigator = () => {
 };
 
 const styles = StyleSheet.create({
-  tabBarContainer: {
-    position: 'absolute',
-    bottom: 25,
-    left: 20,
-    right: 20,
-    elevation: 0,
-    backgroundColor: '#ffffff',
-    borderRadius: 15,
-    height: 90,
-  },
-  shadow: {
-    shadowColor: '#7F5DF0',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5,
-  },
   iconContainer: {
     alignItems: 'center',
   },
